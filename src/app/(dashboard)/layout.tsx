ce4c405a633006f1e1ccb66518c24bba72270a63
Xuemei Huang <mei.huang9500@gmail.com>
2024-08-20 16:52:03 +0900

    add icon files

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -6,8 +10,15 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* left */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        left
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+        <Link
+          href={"/"}
+          className="flex items-center justify-center lg:justify-start"
+        >
+          {/* <Image src={"/logo.png"} alt={"logo"} width={32} height={32} /> */}
+          <span className="hidden lg:block">SchoolBun</span>
+        </Link>
+        <Menu />
       </div>
       {/* right */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
