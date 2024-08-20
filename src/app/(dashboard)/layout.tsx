c068f57165f5e1779a44cc7ea36fb32269f44167
Xuemei Huang <mei.huang9500@gmail.com>
2024-08-20 17:49:02 +0900

    add menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,18 +10,18 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* left */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link
           href={"/"}
-          className="flex items-center justify-center lg:justify-start"
+          className="flex items-center justify-center lg:justify-start gap-2"
         >
-          {/* <Image src={"/logo.png"} alt={"logo"} width={32} height={32} /> */}
-          <span className="hidden lg:block">SchoolBun</span>
+          <Image src={"/b.png"} alt={"logo"} width={32} height={32} />
+          <span className="hidden lg:block">Bolobolo</span>
         </Link>
         <Menu />
       </div>
       {/* right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
         right
       </div>
     </div>
