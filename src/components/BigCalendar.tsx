43eb07c2359708c6f97f0f1be73025163598dbd6
Xuemei Huang <mei.huang9500@gmail.com>
2024-08-28 16:21:19 +0900

    add student and parent page

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -19,7 +19,7 @@ const BigCalendar = () => {
       endAccessor="end"
       views={["month", "work_week", "day"]}
       view={view}
-      style={{ height: "90%" }}
+      style={{ height: "98%" }}
       onView={handleOnChangeView}
       min={new Date(2025, 1, 0, 8, 0, 0)}
       max={new Date(2025, 1, 0, 17, 0, 0)}
