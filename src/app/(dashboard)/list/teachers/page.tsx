b341237748bcb3241c6bbaa3917581109da8a27e
Xuemei Huang <mei.huang9500@gmail.com>
2024-08-28 16:59:56 +0900

    add teacher list page and tablesearch component

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,35 @@
+import TableSearch from "@/components/TableSearch";
+import Image from "next/image";
+
+const TeacherListPage = () => {
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* top */}
+      <div className="flex justify-between items-center">
+        <h1 className="hidden md:block placeholder:text-lg font-semibold">
+          All Teachers
+        </h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+              <Image src={"/filter.png"} alt={""} width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+              <Image src={"/sort.png"} alt={""} width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+              <Image src={"/create.png"} alt={""} width={14} height={14} />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* list */}
+      <div className=""></div>
+      {/* pagenation */}
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default TeacherListPage;
