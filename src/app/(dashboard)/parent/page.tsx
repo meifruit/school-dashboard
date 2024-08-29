103d826a59574edfc9d00f4e97994fbe1197d273
Xuemei Huang <mei.huang9500@gmail.com>
2024-08-29 16:17:25 +0900

    add exam list page

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,121 +1,23 @@
-import Pagination from "@/components/Pagination";
-import Table from "@/components/Table";
-import TableSearch from "@/components/TableSearch";
-import { role, studentsData, teachersData } from "@/lib/data";
-import Image from "next/image";
-import Link from "next/link";
+"use client";
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
 
-type Student = {
-  id: number;
-  studentId: string;
-  name: string;
-  email?: string;
-  photo: string;
-  phone?: string;
-  grade: number;
-  class: string;
-  address: string;
-};
-const columns = [
-  {
-    header: "Info",
-    accessor: "info",
-  },
-  {
-    header: "Student ID",
-    accessor: "studentId",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Grade",
-    accessor: "grade",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Phone",
-    accessor: "phone",
-    className: "hidden lg:table-cell",
-  },
-  {
-    header: "Address",
-    accessor: "address",
-    className: "hidden lg:table-cell",
-  },
-  {
-    header: "Actions",
-    accessor: "action",
-  },
-];
-const StudentListPage = () => {
-  const renderRow = (item: Student) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200 cursor-pointer text-sm hover:bg-purplelight"
-    >
-      <td className="flex items-center gap-4 p-4">
-        <Image
-          src={item.photo}
-          alt={""}
-          width={40}
-          height={40}
-          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
-        />
-        <div className="flex flex-col">
-          <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item?.class}</p>
-        </div>
-      </td>
-      <td className="hidden md:table-cell">{item.studentId}</td>
-      <td className="hidden md:table-cell">{item.grade}</td>
-      <td className="hidden md:table-cell">{item.phone}</td>
-      <td className="hidden md:table-cell">{item.address}</td>
-      <td className="">
-        <div className="flex items-center gap-2">
-          <Link href={`/list/students/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
-              <Image src={"/view.png"} alt={""} width={16} height={16} />
-            </button>
-          </Link>
-          {role === "admin" && (
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
-              <Image src={"/view.png"} alt={""} width={16} height={16} />
-            </button>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
-
+const ParentPage = () => {
   return (
-    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
-      {/* top */}
-      <div className="flex justify-between items-center">
-        <h1 className="hidden md:block placeholder:text-lg font-semibold">
-          All Students
-        </h1>
-        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
-          <TableSearch />
-          <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
-              <Image src={"/filter.png"} alt={""} width={14} height={14} />
-            </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
-              <Image src={"/sort.png"} alt={""} width={14} height={14} />
-            </button>
-            {role === "admin" && (
-              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
-                <Image src={"/create.png"} alt={""} width={14} height={14} />
-              </button>
-            )}
-          </div>
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
+          <BigCalendar />
         </div>
       </div>
-      {/* list */}
-      <Table columns={columns} renderRow={renderRow} data={studentsData} />
-      {/* pagenation */}
-      <Pagination />
+      {/* right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
     </div>
   );
 };
 
-export default StudentListPage;
+export default ParentPage;
