aab693fa1d9d8cbed7ed497cc67effacac360045
Xuemei Huang <mei.huang9500@gmail.com>
2024-08-30 16:10:29 +0900

    add teacher single page

---
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,148 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import Image from "next/image";
+import Link from "next/link";
+
+const SingleTeacherPage = () => {
+  return (
+    <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
+      {/* left */}
+      <div className="w-full xl:w-2/3">
+        {/* top */}
+        <div className="flex flex-col lg:flex-row gap-4">
+          {/* user info card */}
+          <div className="bg-sky py-6 px-4 rounded-md flex-1 flex gap-4">
+            <div className="w-1/3">
+              <Image
+                src={
+                  "https://images.pexels.com/photos/27514901/pexels-photo-27514901.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                }
+                alt={""}
+                width={96}
+                height={96}
+                className="w-24 h-24 rounded-full object-cover"
+              />
+            </div>
+            <div className="w-2/3 flex flex-col justify-between gap-4">
+              <div className="flex items-center gap-4">
+                <h1 className="text-xl font-semibold">Bur bur</h1>
+              </div>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </p>
+              <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src={"/blood.png"} alt={""} width={14} height={14} />
+                  <span>A+</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src={"/date.png"} alt={""} width={14} height={14} />
+                  <span>Jan 2025</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src={"/mail.png"} alt={""} width={14} height={14} />
+                  <span>abc@gmail.com</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src={"/phone.png"} alt={""} width={14} height={14} />
+                  <span>12345678</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* small cards */}
+          <div className="flex-1 flex gap-4 justify-between flex-wrap">
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleBranch.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">2</h1>
+                <span className="text-sm text-gray-400">Branches</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleLesson.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-400">Lessons</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleClass.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-400">Classes</span>
+              </div>
+            </div>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleClass.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-400">Classes</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* bottom */}
+        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+          <h1 className="">Teacher&apos;s Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+        <div className="bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          {/* shortcuts */}
+          <div className="flex flex-wrap items-start mt-4 gap-4 text-xs text-gray-500">
+            <Link href={""} className="bg-skylight rounded-md p-3">
+              Teacher&apos;s Classes
+            </Link>
+            <Link href={""} className="bg-purplelight rounded-md p-3">
+              Teacher&apos;s Classes
+            </Link>
+            <Link href={""} className="bg-pink-50 rounded-md p-3">
+              Teacher&apos;s Classes
+            </Link>
+            <Link href={""} className="bg-skylight rounded-md p-3">
+              Teacher&apos;s Classes
+            </Link>
+          </div>
+        </div>
+        {/* performance */}
+
+        {/* announcement */}
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default SingleTeacherPage;
