92e77e36c5766c47e1ff99f87ba2107ec6657fe1
Xuemei Huang <mei.huang9500@gmail.com>
2024-09-03 20:51:56 +0900

    add FormModal and teacher form

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,6 +1,8 @@
 import Announcements from "@/components/Announcements";
 import BigCalendar from "@/components/BigCalendar";
+import FormModal from "@/components/FormModal";
 import Performance from "@/components/Performance";
+import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -27,6 +29,26 @@ const SingleTeacherPage = () => {
             <div className="w-2/3 flex flex-col justify-between gap-4">
               <div className="flex items-center gap-4">
                 <h1 className="text-xl font-semibold">Bur bur</h1>
+                {role === "admin" && (
+                  <FormModal
+                    table="teacher"
+                    type="update"
+                    data={{
+                      id: 1,
+                      username: "deanguerrero",
+                      email: "deanguerrero@gmail.com",
+                      password: "password",
+                      firstName: "Dean",
+                      lastName: "Guerrero",
+                      phone: "+1 234 567 89",
+                      address: "1234 Main St, Anytown, USA",
+                      bloodType: "A+",
+                      dateOfBirth: "2000-01-01",
+                      sex: "male",
+                      img: "https://images.pexels.com/photos/27514901/pexels-photo-27514901.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+                    }}
+                  />
+                )}
               </div>
               <p className="text-sm text-gray-500">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
