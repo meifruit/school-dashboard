f40c859c9dc0fa0ec3afd1305c486a86e533ca8e
Xuemei Huang <mei.huang9500@gmail.com>
2024-10-15 13:44:05 +0900

    changed menu background color

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -121,7 +121,7 @@ const Menu = () => {
     <div className="mt-4 text-sm">
       {menuItems.map((i) => (
         <div className="flex flex-col gap-2" key={i.title}>
-          <span className="hidden lg:block text-gray-400 font-light my-4">
+          <span className="hidden lg:block text-gray-300 font-light my-4">
             {i.title}
           </span>
           {i.items.map((item) => {
@@ -130,7 +130,7 @@ const Menu = () => {
                 <Link
                   href={item.href}
                   key={item.label}
-                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-skylight"
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-400 py-2 md:px-2 rounded-md hover:bg-skylight"
                 >
                   <Image
                     src={item.icon}
