3b752bcb03a1807862abd4dd8e9cc02440cdcb56
Xuemei Huang <mei.huang9500@gmail.com>
2024-11-23 11:39:28 +0900

    add subjectform

---
+++ src/components/forms/SubjectForm.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import InputField from "../InputField";
+import Image from "next/image";
+import { subjectSchema, SubjectSchema } from "@/lib/formValidationSchema";
+
+const SubjectForm = ({
+  type,
+  data,
+}: {
+  type: "create" | "update";
+  data?: any;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SubjectSchema>({
+    resolver: zodResolver(subjectSchema),
+  });
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+  return (
+    <form className="flex flex-col gap-8" onSubmit={onSubmit}>
+      <h1 className="text-xl font-semibold">
+        {type === "create" ? "Create a new subject" : "Update the subject"}
+      </h1>
+
+      <div className="flex justify-between flex-wrap gap-4">
+        <InputField
+          label={"Subject name"}
+          register={register}
+          name={"name"}
+          defaultValue={data?.username}
+          error={errors?.name}
+        />
+      </div>
+      <button className="bg-blue-400 text-white p-2 rounded-md">
+        {type === "create" ? "Create" : "Update"}
+      </button>
+    </form>
+  );
+};
+
+export default SubjectForm;
