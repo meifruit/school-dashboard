4b8a6d55f152f8a99a3b52f70f6441991e17f66b
Xuemei Huang <mei.huang9500@gmail.com>
2024-11-25 16:03:51 +0900

    add update and delete subject

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -22,3 +22,42 @@ export const createSubject = async (
     return { success: false, error: true };
   }
 };
+
+export const updateSubject = async (
+  currentState: CurrentState,
+  data: SubjectSchema
+) => {
+  try {
+    await prisma.subject.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        name: data.name,
+      },
+    });
+    // revalidatePath("/list/subjects");
+    return { success: true, error: false };
+  } catch (err) {
+    console.log(err);
+    return { success: false, error: true };
+  }
+};
+export const deleteSubject = async (
+  currentState: CurrentState,
+  data: FormData
+) => {
+  const id = data.get("id") as string;
+  try {
+    await prisma.subject.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    // revalidatePath("/list/subjects");
+    return { success: true, error: false };
+  } catch (err) {
+    console.log(err);
+    return { success: false, error: true };
+  }
+};
