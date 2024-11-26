bd32d821a4a666c9d9dfc0d229d5cc0a15e1401d
Xuemei Huang <mei.huang9500@gmail.com>
2024-11-26 16:48:44 +0900

    fetch teachers create update delete form and add cloudinary

--- src/lib/formValidationSchema.ts
+++ src/lib/formValidationSchema.ts
@@ -15,3 +15,29 @@ export const classSchema = z.object({
   supervisorId: z.coerce.string().optional(),
 });
 export type ClassSchema = z.infer<typeof classSchema>;
+
+export const teacherSchema = z.object({
+  id: z.string().optional(),
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long." })
+    .max(20, { message: "Username must be at most 20 characters long." }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+  name: z.string().min(1, { message: "First name is required." }),
+  surname: z.string().min(1, { message: "Last name is required!" }),
+  email: z
+    .string()
+    .email({ message: "Invalid email address." })
+    .optional()
+    .or(z.literal("")),
+  phone: z.string().optional(),
+  address: z.string().optional(),
+  img: z.string().optional(),
+  bloodType: z.string().min(1, { message: "Blood Type is required!" }),
+  birthday: z.date({ message: "Birthday is required!" }),
+  sex: z.enum(["MALE", "FEMALE"], { message: "Sex is required!" }),
+  subjects: z.array(z.string()).optional(), // subject ids
+});
+export type TeacherSchema = z.infer<typeof teacherSchema>;
