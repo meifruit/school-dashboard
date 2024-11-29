b65a046808b8d9300d9e135b2712e1b145ab66dc
Xuemei Huang <mei.huang9500@gmail.com>
2024-11-29 22:34:19 +0900

    fetch the attendance percentage and bigcalendar

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,23 +1,30 @@
 "use client";
-import { Calendar, View, Views, momentLocalizer } from "react-big-calendar";
+
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
 import moment from "moment";
-import { calendarEvents } from "@/lib/data";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from "react";
+
 const localizer = momentLocalizer(moment);
 
-const BigCalendar = () => {
+const BigCalendar = ({
+  data,
+}: {
+  data: { title: string; start: Date; end: Date }[];
+}) => {
   const [view, setView] = useState<View>(Views.WORK_WEEK);
+
   const handleOnChangeView = (selectedView: View) => {
     setView(selectedView);
   };
+
   return (
     <Calendar
       localizer={localizer}
-      events={calendarEvents}
+      events={data}
       startAccessor="start"
       endAccessor="end"
-      views={["month", "work_week", "day"]}
+      views={["work_week", "day"]}
       view={view}
       style={{ height: "98%" }}
       onView={handleOnChangeView}
@@ -26,4 +33,5 @@ const BigCalendar = () => {
     />
   );
 };
+
 export default BigCalendar;
