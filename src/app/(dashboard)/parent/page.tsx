5cdc56cf4787d11ec93416ffab6383e2d55fa44c
Xuemei Huang <mei.huang9500@gmail.com>
2024-12-01 15:49:15 +0900

    fetch student page:

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,39 +1,36 @@
 import Announcements from "@/components/Announcements";
 import BigCalendarContainer from "@/components/BigCalendarContainer";
+import EventCalendar from "@/components/EventCalendar";
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 
-const ParentPage = async () => {
-  const authOject = await auth();
-  const { userId } = authOject;
-  const currentUserId = userId;
-  const students = await prisma.student.findMany({
+const StudentPage = async () => {
+  const authObject = await auth();
+  const { userId } = authObject;
+
+  const classItem = await prisma.class.findMany({
     where: {
-      parentId: currentUserId!,
+      students: { some: { id: userId! } },
     },
   });
 
+  console.log(classItem);
   return (
-    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
       {/* LEFT */}
-      <div className="">
-        {students.map((student) => (
-          <div className="w-full" key={student.id}>
-            <div className="h-full bg-white p-4 rounded-md">
-              <h1 className="text-xl font-semibold">
-                Schedule ({student.name + " " + student.surname})
-              </h1>
-              <BigCalendarContainer type="classId" id={student.classId} />
-            </div>
-          </div>
-        ))}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendarContainer type="classId" id={classItem[0].id} />
+        </div>
       </div>
       {/* RIGHT */}
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
         <Announcements />
       </div>
     </div>
   );
 };
 
-export default ParentPage;
+export default StudentPage;
