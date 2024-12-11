9557caecbb329c3497a94605d1d901a54cb9cd96
Xuemei Huang <mei.huang9500@gmail.com>
2024-12-11 18:11:10 +0900

    fetch announcement for home page

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -87,19 +87,19 @@ const AnnouncementListPage = async ({
     }
   }
 
-  // const roleConditions = {
-  //   teacher: { lessons: { some: { teacherId: currentUserId! } } },
-  //   student: { students: { some: { id: currentUserId! } } },
-  //   parent: { students: { some: { parentId: currentUserId! } } },
-  // };
-  // query.OR = [
-  //   {
-  //     classId: null,
-  //   },
-  //   {
-  //     class: roleConditions[role as keyof typeof roleConditions] || {},
-  //   },
-  // ];
+  const roleConditions = {
+    teacher: { lessons: { some: { teacherId: currentUserId! } } },
+    student: { students: { some: { id: currentUserId! } } },
+    parent: { students: { some: { parentId: currentUserId! } } },
+  };
+  query.OR = [
+    {
+      classId: null,
+    },
+    {
+      class: roleConditions[role as keyof typeof roleConditions] || {},
+    },
+  ];
   const [announcements, count] = await prisma.$transaction([
     prisma.announcement.findMany({
       where: query,
