b578bc4f85afe4b53abfe42e29f01afe37062119
Xuemei Huang <mei.huang9500@gmail.com>
2024-12-11 18:25:37 +0900

    fetch user card for admin page

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,18 @@
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 
-const UserCard = ({ type }: { type: string }) => {
+const UserCard = async ({
+  type,
+}: {
+  type: "admin" | "teacher" | "parent" | "student";
+}) => {
+  const modelMap: Record<typeof type, any> = {
+    admin: prisma.admin,
+    teacher: prisma.teacher,
+    student: prisma.student,
+    parent: prisma.parent,
+  };
+  const data = await modelMap[type].count();
   return (
     <div className="rounded-2xl p-4 flex-1 min-w-[130px] bg-white">
       <div className="flex flex-col gap-2">
@@ -10,7 +22,7 @@ const UserCard = ({ type }: { type: string }) => {
           </span>
           <Image src={"/more.png"} alt={""} width={20} height={20} />
         </div>
-        <h1 className="text-2xl font-semibold my-4">1,234</h1>
+        <h1 className="text-2xl font-semibold my-4">{data}</h1>
         <h2 className="text-sm font-medium text-gray-500">{type}s</h2>
       </div>
     </div>
